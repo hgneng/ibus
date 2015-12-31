80542fd5616ed0d2b010fa78095c634885d90e64
Cameron Wong <hgneng@gmail.com>
2015-12-31 12:26:51 +0800

    alpha release

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/bash
-sudo apt-get install -y gnome-common gtk-doc-tools libdconf-dev libnotify-dev valac
+#!/bin/bash
+sudo apt-get install -y gnome-common gtk-doc-tools libdconf-dev libnotify-dev valac libgtk-3-dev
 sudo apt-get install -y libspeechd-dev
 sudo apt-get install -y ibus-pinyin
 
 ./autogen.sh
 
-./configure --prefix=/usr --libexecdir=/usr/lib/ibus
+./configure --prefix=/usr --libexecdir=/usr/lib/ibus --disable-gtk2
 
 make
 
 sudo make install
 
-im-switch -s ibus
+ui/gtk3/ibus-ui-gtk3&
+sleep 3
+sudo cp ui/gtk3/.libs/lt-ibus-ui-gtk3 /usr/lib/ibus/ibus-ui-gtk3
+
+im-config -n ibus
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'pinyin')]"
+ibus-daemon -drx
 ibus restart
